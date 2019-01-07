a87f0e87b70e55085d26257722097a52fa47f39d
Ryan Sevey <sevey1@gmail.com>
2019-01-07 14:24:58 -0500

    added search and styles

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,18 +1,9 @@
 class Stock < ApplicationRecord
 
     def self.new_from_lookup(ticker_symbol)
-        puts '========='
-        p ticker_symbol
-        begin
             looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-            p looked_up_stock
             price = looked_up_stock.latest_price
-            p price
             new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
-        rescue Exception => e
-            puts '----rescue----'
-            nil
-          end
         end
     end
 
