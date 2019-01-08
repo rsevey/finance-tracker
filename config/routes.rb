8174d0c06202b5455ad049f16c280f7255e02bde
Ryan Sevey <sevey1@gmail.com>
2019-01-08 14:57:43 -0800

    added destroy method to user stocks

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ root 'welcome#index'
 get 'my_portfolio', to: "users#my_portfolio"
 get 'search_stocks', to: 'stocks#search'
 
-resources :user_stocks, only: [:create]
+resources :user_stocks, only: [:create, :destroy]
 end
